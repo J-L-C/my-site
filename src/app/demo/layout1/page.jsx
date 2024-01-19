7103f5ea0aed3dedacace7201ad28e2fcd51e836
Jesse Cooper <J-L-C@users.noreply.github.com>
2024-01-18 20:24:06 -0600

    Simple working layout of my picture and an empty container div

---
+++ src/app/demo/layout1/page.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import {Container} from "postcss";
+
+export default function Layout1(){
+
+    return(
+        <>
+            <h1>This is my first experiment</h1>
+            <div className={"flex justify-center relative top-0"}>
+                <div className={"bg-amber-500 sm:w-1/2 w-full h-screen absolute top-5 rounded-lg"}>
+                    <Image
+                        className={"shadow-lg rounded-lg absolute -left-3 -top-3"}
+                        src={"/me.png"}
+                        alt={"A picture of myself smiling into the camera"}
+                        width={200}
+                        height={200}
+                    />
+                    this is a container
+
+                </div>
+
+            </div>
+        </>
+    )
+}
