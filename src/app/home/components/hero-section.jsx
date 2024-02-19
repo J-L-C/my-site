238c6fcbea8da90907048adbb05ce1a053efe2a4
Jesse Cooper <J-L-C@users.noreply.github.com>
2024-02-19 10:59:21 -0600

    Update about me

--- src/app/home/components/hero-section.jsx
+++ src/app/home/components/hero-section.jsx
@@ -10,7 +10,7 @@ export default function HeroSection({className}){
             <div className="justify-center items-center">
 
                 <p className={"text-white p-8 text-justify bg-[#1182d4]"}>
-                    I am Full Stack Engineer by trade and with a lot of consulting experience. I have tackled
+                    I am Full Stack Engineer by trade and with many years of consulting experience. I have tackled
                     unique challenges across many different industries. It is exciting to be apart of a product&apos;s journey
                     from inception to delivery and that&apos;s were I thrive. Beyond software, I find myself exploring more
                     hobbies than I have time for and enjoy them all!
